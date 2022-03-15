5da6815618b06f5b1006bc8ad9eac2ac3f6bc469
sanghakyi <sang105kr@gmail.com>
2022-03-15 11:29:28 +0900

    220315-3

--- src/main/java/com/kh/app3/web/BbsController.java
+++ src/main/java/com/kh/app3/web/BbsController.java
@@ -1,16 +1,17 @@
 package com.kh.app3.web;
 
+import com.kh.app3.domain.bbs.dao.Bbs;
 import com.kh.app3.domain.bbs.svc.BbsSVC;
 import com.kh.app3.domain.common.CodeDAO;
 import com.kh.app3.web.form.bbs.AddForm;
+import com.kh.app3.web.form.bbs.DetailForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -41,8 +42,23 @@ public class BbsController {
 
   //작성처리
   @PostMapping("/add")
-  public String add(@ModelAttribute AddForm addForm) {
+  public String add(
+      @ModelAttribute AddForm addForm,
+      RedirectAttributes redirectAttributes) {
+    log.info("addForm={}",addForm);
 
+    Bbs bbs = new Bbs();
+//    bbs.setBcategory(addForm.getBcategory());
+//    bbs.setTitle(addForm.getTitle());
+//    bbs.setEmail(addForm.getEmail());
+//    bbs.setNickname(addForm.getNickname());
+//    bbs.setBcontent(addForm.getBcontent());
+    BeanUtils.copyProperties(addForm, bbs);
+
+    Long originId = bbsSvc.saveOrigin(bbs);
+    redirectAttributes.addAttribute("id", originId);
+    // <=서버응답 302 get http://서버:port/bbs/10
+    // =>클라이언트요청 get http://서버:port/bbs/10
     return "redirect:/bbs/{id}";
   }
 
@@ -55,9 +71,23 @@ public class BbsController {
 
   //조회
   @GetMapping("/{id}")
-  public String detail() {
+  public String detail(
+      @PathVariable Long id,
+      Model model) {
 
-    return "bbs/detail";
+    Bbs detailBbs = bbsSvc.findByBbsId(id);
+
+    DetailForm detailForm = new DetailForm();
+//    detailForm.setBcategory(detailBbs.getBcategory());
+//    detailForm.setTitle(detailBbs.getTitle());
+//    detailForm.setBcontent(detailBbs.getBcontent());
+//    detailForm.setEmail(detailBbs.getEmail());
+//    detailForm.setNickname(detailBbs.getNickname());
+//    detailForm.setHit(detailBbs.getHit());
+    BeanUtils.copyProperties(detailBbs, detailForm);
+    model.addAttribute("detailForm", detailForm);
+
+    return "bbs/detailForm";
   }
 
   //삭제
