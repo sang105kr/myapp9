501d5a68a818e26f7906289d271334170a382df7
sanghakyi <sang105kr@gmail.com>
2022-03-17 14:42:59 +0900

    220317-5

--- src/main/java/com/kh/app3/web/BbsController.java
+++ src/main/java/com/kh/app3/web/BbsController.java
@@ -3,10 +3,7 @@ package com.kh.app3.web;
 import com.kh.app3.domain.bbs.dao.Bbs;
 import com.kh.app3.domain.bbs.svc.BbsSVC;
 import com.kh.app3.domain.common.CodeDAO;
-import com.kh.app3.web.form.bbs.AddForm;
-import com.kh.app3.web.form.bbs.DetailForm;
-import com.kh.app3.web.form.bbs.EditForm;
-import com.kh.app3.web.form.bbs.ListForm;
+import com.kh.app3.web.form.bbs.*;
 import com.kh.app3.web.form.login.LoginMember;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -184,14 +181,58 @@ public class BbsController {
 
   //답글작성양식
   @GetMapping("/{id}/reply")
-  public String replyForm() {
+  public String replyForm(@PathVariable Long id,
+                          Model model,HttpSession session) {
+    Bbs parentBbs = bbsSvc.findByBbsId(id);
+    ReplyForm replyForm = new ReplyForm();
+    replyForm.setBcategory(parentBbs.getBcategory());
+    replyForm.setTitle("답글:"+parentBbs.getTitle());
+
+    //세션에서 로그인정보 가져오기
+    LoginMember loginMember = (LoginMember)session.getAttribute(SessionConst.LOGIN_MEMBER);
+    replyForm.setEmail(loginMember.getEmail());
+    replyForm.setNickname(loginMember.getNickname());
+
+    model.addAttribute("replyForm", replyForm);
     return "bbs/replyForm";
   }
 
   //답글작성처리
   @PostMapping("/{id}/reply")
-  public String reply(){
+  public String reply(
+    @PathVariable Long id,      //부모글의 bbsId
+    @Valid ReplyForm replyForm,
+    BindingResult bindingResult,
+    RedirectAttributes redirectAttributes
+                      ){
+    if(bindingResult.hasErrors()){
+      return "bbs/replyForm";
+    }
 
+    Bbs replyBbs = new Bbs();
+    BeanUtils.copyProperties(replyForm, replyBbs);
+
+    //부모글의 bcategory,bbsId,bgroup,step,bindent 참조
+    appendInfoOfParentBbs(id, replyBbs);
+
+    //답글저장(return 답글번호)
+    Long replyBbsId = bbsSvc.saveReply(id, replyBbs);
+
+    redirectAttributes.addAttribute("id",replyBbsId);
     return "redirect:/bbs/{id}";
   }
+
+  //부모글의 bbsId,bgroup,step,bindent
+  private void appendInfoOfParentBbs(Long parentBbsId, Bbs replyBbs) {
+    Bbs parentBbs = bbsSvc.findByBbsId(parentBbsId);
+    replyBbs.setBcategory(parentBbs.getBcategory());
+    replyBbs.setPbbsId(parentBbs.getPbbsId());
+    replyBbs.setBgroup(parentBbs.getBgroup());
+    replyBbs.setStep(parentBbs.getStep());
+    replyBbs.setBindent(parentBbs.getBindent());
+  }
 }
+
+
+
+
